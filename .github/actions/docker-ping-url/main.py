1fb3eabb0df522b432d045e1037d315e57a10310
Samuel Pineau <samuel.pineau.ext@coptis.com>
2025-05-30 13:37:50 +0200

    workflow "17 - 3 - Custom Actions - Docker" - Improve workflow and add logic into python code

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,35 @@
+import requests
+import time
+
+# declare a function
+def ping_url(url, delay, max_trials):
+    trial = 0
+
+    while (trial < max_trials):
+        result = requests.get(url)
+
+        if(result.status_code == 200):
+            return True
+        else:
+            time.sleep(delay)
+    
+    return False
+
+
+def run():
+    # Get action.yaml inputs
+    url = INPUT_URL
+    delay = INPUT_DELAY
+    max_trials = INPUT_MAX_TRIALS
+
+    print("Start ping url '{url}' with max trials '{max_trials}' and a delay of '{delay}' between trials.")
+
+    result = ping_url(url, delay, max_trials)
+
+    if(result == False):
+        raise ValueError("Impossible to ping url '{url}' after {max_trials} trials!")
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    run()
+        
